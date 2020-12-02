f6eff52e05cab25fe2b1af47bf7082da28d01d91
NH <izenparadox@gmail.com>
2020-12-02 07:05:32 +0000

    Added greet function with input.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-# Review: 
-# Create a function called greet(). 
-# Write 3 print statements inside the function.
-# Call the greet() function and run your code.
+# Caesar Cipher challenge
 
+# simple greet function
 def greet():
   print('Welcome!')
   print('Willkommen!')
   print('Bienvenido!')
 
+# greet function with input
+def greet_this_person(name):
+  print(f'\nWelcome {name}!')
+  print(f'How are you today {name}?')
+
 greet()
+greet_this_person('Grogu')
