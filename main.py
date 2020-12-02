12fc58c69e6076906b2a7d26010edac227468c2e
NH <izenparadox@gmail.com>
2020-12-02 06:47:23 +0000

    Created a greet function.

--- main.py
+++ main.py
@@ -3,4 +3,9 @@
 # Write 3 print statements inside the function.
 # Call the greet() function and run your code.
 
+def greet():
+  print('Welcome!')
+  print('Willkommen!')
+  print('Bienvenido!')
 
+greet()
