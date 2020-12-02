0bcd6adc3eeb2f3108a7098721d637e00f50c547
NH <izenparadox@gmail.com>
2020-12-02 07:26:24 +0000

    added a functions with 2 inputs using positional & keyword arguments

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 # Caesar Cipher challenge
 
-# simple greet function
+#simple greet function
 def greet():
   print('Welcome!')
   print('Willkommen!')
   print('Bienvenido!')
 
-# greet function with input
+#function with input
 def greet_this_person(name):
   print(f'\nWelcome {name}!')
   print(f'How are you today {name}?')
 
-greet()
-greet_this_person('Grogu')
+# greet()
+# greet_this_person('Grogu')
+
+#function with 2 inputs using positional arguments
+def greet_with(name,location):
+  print(f'Hello {name}.')
+  print(f'What is it like in {location}?')
+
+greet_with('Grogu','Tattooine')
+
+#function with 2 inputs using keyword arguments
+greet_with(location='Tattooine', name='Grogu')
