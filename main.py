a3e8a5274878e2293061f397dad1e62290466176
NH <izenparadox@gmail.com>
2020-12-04 23:44:44 +0000

    Added function to calculate prime numbers

--- main.py
+++ main.py
@@ -45,3 +45,20 @@ coverage = 5
 paint_calc(height=test_h, width=test_w, cover=coverage)
 
 
+#Write your code below this line 👇
+
+def prime_checker(number):
+  for n in range(2, number):
+    if (number % n == 0):
+      print(f"n={n} The number {number} is not a prime")
+     
+    else:
+      print(f"#{n} It is a prime number")
+  
+            
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
